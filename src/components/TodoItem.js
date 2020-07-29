a92dbb09b12f56e9657468d6294c42eec42cfe33
Vikranth19 <64738319+Vikranth19@users.noreply.github.com>
2020-07-29 11:12:23 -0700

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ class TodoItem extends Component {
     render() {
         const {title,handleDelete,handleEdit} =this.props
         return (
-            <li className="list-group-item text-capitalize d-flex justify-content-between my-2 todo-item">
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
                 <h6>{title}</h6>
                 <div className="todo-icon">
                     <span className="mx-2 text-success" onClick={handleEdit}>
